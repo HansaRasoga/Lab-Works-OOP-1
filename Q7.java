00f066319244fb6ba03ed934c4967acb84454165
HansaRasoga <hansarasoga@gmail.com>
2025-06-20 20:56:17 +0530

    Adding code

---
+++ Q7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Q7 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get temperature in Celsius from user
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        // Convert to Fahrenheit
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        // Display result
+        System.out.printf("Temperature in Fahrenheit = "+ fahrenheit);
+
+        scanner.close();
+    }
+}
