86ab081950fac1ab2b03430cf94ce63b278c3ede
HansaRasoga <hansarasoga@gmail.com>
2025-06-20 14:42:43 +0530

    Adding code

---
+++ PrintBill.java
@@ -0,0 +1,13 @@
+public class PrintBill {
+    public static void main(String [] args)
+    {
+        System.out.println("USER INFORMATION");
+        System.out.println("--------------------");
+        System.out.println("NAME:\t SUNIL SILVA");
+        System.out.println("ADDRESS: No23, Panadura");
+        System.out.println("AGE:\t 19");
+        System.out.println("SALARY:\t 23500");
+        System.out.println("GENDER:\t M");
+        System.out.println("--------------------");
+    }
+}
