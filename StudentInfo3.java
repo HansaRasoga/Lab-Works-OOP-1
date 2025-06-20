441846c5d1a5825b9d3787e5dd6430a6c48a82aa
HansaRasoga <hansarasoga@gmail.com>
2025-06-20 15:10:36 +0530

    Adding code

---
+++ StudentInfo3.java
@@ -0,0 +1,16 @@
+public class StudentInfo3 {
+    public static void main(String[] args) {
+    String name="Hansa Rasoga";
+    String address="No20/1, Sri Wijayarama Lane, Kaluwadumulla, Ambalangoda";
+    int age=22;
+    
+        System.out.println("STUDENT INFORMATION");
+        System.out.println("--------------------");
+        System.out.println("Name:\t"+name);
+        System.out.println("Address:"+address);
+        System.out.println("Age:\t"+age);
+        
+    }
+    
+    }
+
