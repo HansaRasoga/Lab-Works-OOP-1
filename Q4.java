078334dcefa5ab72921daaa204e3e45355bde01c
HansaRasoga <hansarasoga@gmail.com>
2025-06-20 15:20:34 +0530

    Adding code

---
+++ Q4.java
@@ -0,0 +1,18 @@
+public class Q4 {
+    public static void main(String [] args)
+    {
+    double a=-1 + 4 * 6;
+
+    double b=(35+ 5 ) % 7;
+
+    double c=14 + -4 * 6 / 11;
+
+    double d=2 + 15 / 6 * 1 - 7 % 2;
+
+    System.out.println("-1 + 4 * 6 \t="+a);
+    System.out.println("(35 + 5) % 7 \t="+b);
+    System.out.println("14 + -4 * 6 / 11 ="+c);
+    System.out.println("2 + 15 / 6 * 1 - 7 % 2 ="+d);
+
+    }
+}
